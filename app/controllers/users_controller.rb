853f0877a5b43a13dd7c74c1daa9decab35e86e5
Le Hoa <lehoa543@gmail.com>
2017-12-11 16:49:00 +0700

    finish chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by_id params[:id]
+    unless @user
+      flash[:danger] = t :notfind
+      redirect_to root_url
+    end
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t :sample
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
