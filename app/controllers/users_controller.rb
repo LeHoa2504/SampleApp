d749fd24a872a5c7fb3e15d288522a3cc5ce9560
Le Hoa <lehoa543@gmail.com>
2017-12-11 16:49:00 +0700

    finish chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,17 +15,17 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-
       log_in @user
       flash[:success] = t :sample
       redirect_to @user
     else
+
+      flash[:danger] = t :notcreate
       render :new
     end
   end
 
   private
-
   def user_params
       params.require(:user).permit :name, :email, :password, :password_confirmation
   end
